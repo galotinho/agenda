fac74e9b8cd38ddd69f84f73140829cf971612e0
Rafael Lopes <rafael.lopes@univision.net.br>
2022-02-09 08:58:37 -0300

    Inclusão do Put - Update

--- src/main/java/com/agenda/api/AgendaController.java
+++ src/main/java/com/agenda/api/AgendaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,5 +48,16 @@ public class AgendaController {
 	@DeleteMapping("/{id}")
 	public void deletar(@PathVariable Long id) {
 		repository.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public void atualizar(@RequestBody Agenda contatoNovo, @PathVariable Long id) {
+		Agenda contatoVelho = repository.findById(id).get();
+		
+		contatoVelho.setNome(contatoNovo.getNome());
+		contatoVelho.setTelefone(contatoNovo.getTelefone());
+		
+		repository.save(contatoVelho);
+		
 	}
 }
